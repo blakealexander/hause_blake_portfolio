411f6a4dad22fa17a1c3570153f8b5e4d6fad577
blake <blakehause@Blakes-MacBook-Pro.local>
2018-11-11 13:53:24 -0500

    added gulp and animate.css and sass.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie11'}))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('animateCss', function () {
+    return gulp.src('node_modules/animate.css/animate.min.css')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+});
+
+gulp.task('wowJs', function () {
+  return gulp.src('node_modules/wowjs/dist/wow.min.js')
+    .pipe(gulp.dest('./js'));
+});
+
+
+gulp.task('default',['wowJs', 'sass', 'animateCss']);
