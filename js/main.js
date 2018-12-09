df48a11effb6b466cfd28302abd0488a33d053bb
blake <blakehause@Blakes-MacBook-Pro.local>
2018-12-09 14:36:57 -0500

    styled light box, and video box

--- js/main.js
+++ js/main.js
@@ -75,4 +75,73 @@ $(document).ready(function(){
 	})
 });
 
+//----------------------VIDEO PLAYER
+
+// Reel Variables
+
+var reel = document.querySelector("video"),
+    volumeControl = document.querySelector("#volControl"),
+    video = document.querySelector("#videoReel"),
+    muteVideo = document.querySelector("#volMute"),
+    muteBtn = document.querySelector("#muteBtn"),
+    pPlay = document.querySelector("#pPlay"),
+    pPlayBtn = document.querySelector("#pPlayBtn"),
+    seekSlider = document.getElementById("seekSlider");;
+
+//Reel Functions
+
+function pausePlay() {
+    if (reel.paused) {
+        reel.play();
+        pPlayBtn.classList.replace( 'fa-play-circle', 'fa-pause-circle' );
+    } else {
+        reel.pause();
+        pPlayBtn.classList.replace( 'fa-pause-circle', 'fa-play-circle' );
+    }
+}
+
+function changeVol () {
+    if(volumeControl.value == 0){
+        video.muted = true;
+        muteBtn.classList.replace('fa-volume-up', 'fa-volume-mute');
+    } else {
+        video.volume = (volumeControl.value/100);
+        video.muted = false;
+        muteBtn.classList.replace('fa-volume-mute', 'fa-volume-up');
+    }
+	// video.volume = volumeControl.value / 100; 
+	// console.log(volumeControl.value);
+ }
+
+function muteVid(){
+    if(reel.muted){
+        reel.muted = false;
+        volumeControl.value = volumeControlValue;
+        muteBtn.classList.replace('fa-volume-mute', 'fa-volume-up');
+    } else {
+        volumeControlValue = volumeControl.value;
+        volumeControl.value = 0;
+        reel.muted = true;
+        muteBtn.classList.replace('fa-volume-up', 'fa-volume-mute');
+        // console.log( muteBtn.classList.replace('fa-volume-up', 'fa-volume-mute'));
+    }
+}
+
+function videoSeek(){
+	var seekto = video.duration * (seekSlider.value / 100);
+	video.currentTime = seekto;
+}
+function seekTimeUpdate(){
+	var nt = video.currentTime * (100 / video.duration);
+	seekSlider.value = nt;
+}
+
+// Reel Event Listeners
+
+muteVideo.addEventListener("click", muteVid, false);
+pPlay.addEventListener("click", pausePlay, false);
+volumeControl.addEventListener("mouseup", changeVol, false);
+seekSlider.addEventListener("change",videoSeek,false);
+video.addEventListener("timeupdate",seekTimeUpdate,false);
+
 
